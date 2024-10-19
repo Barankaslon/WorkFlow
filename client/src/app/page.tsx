4bb1832dbe7ed449dbe86f41151641f4c5b4f549
Alexey Gvozdev <72240384+Barankaslon@users.noreply.github.com>
2024-10-19 15:14:49 -0300

    Navbar and Search styles

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        hey
+        Hey Kennedy
       </main>
   );
 }
