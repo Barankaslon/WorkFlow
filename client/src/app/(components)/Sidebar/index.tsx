661ab9190d954c7b4cd6ea686db6f8fa21896da5
Alexey Gvozdev <72240384+Barankaslon@users.noreply.github.com>
2024-10-21 10:33:01 -0300

    Sidebar

--- client/src/app/(components)/Sidebar/index.tsx
+++ client/src/app/(components)/Sidebar/index.tsx
@@ -1,17 +1,16 @@
-"use client";
 
-import { LockIcon } from 'lucide-react';
+import { useAppDispatch, useAppSelector } from '@/app/redux';
+import { Home, HomeIcon, Icon, Link, LockIcon, LucideHome, LucideIcon } from 'lucide-react';
 import Image from 'next/image';
+import { usePathname } from 'next/navigation';
 import React, { useState } from 'react'
 
 const Sidebar = () => {
-    const {showProjects, setShowProjects} = useState(true);
-    const {showPriority, setShowPriority} = useState(true);
+    //const {showProjects, setShowProjects} = useState(true);
+    //const {showPriority, setShowPriority} = useState(true);
 
     const sidebarClassNames = `fixed flex flex-col h-[100%] justify-between shadow-xl
-    transition-all duration-300 h-full z-40 dark:bg-black overflow-y-auto bg-white
-    ${isSidebarCollapsed ? "w-0 hidden" : "w-64"}
-  `;
+    transition-all duration-300 h-full z-40 dark:bg-black overflow-y-auto bg-white w-64`;
   return <div className= {sidebarClassNames}>
     <div className='flex h-[100%] w-full flex-col justify-start'>
         {/* TOP LOGO */}
@@ -34,8 +33,55 @@ const Sidebar = () => {
             </div>
         </div>
         {/* NAVBAR LINKS */}
+        <nav className='z-10 w-full'>
+            <SidebarLink 
+            icon={HomeIcon}
+            label='Home'
+            href="/" 
+            />
+        </nav>
     </div>
   </div>
+};
+
+interface SidebarlinkProps {
+    href: string;
+    icon: LucideIcon;
+    label: string;
+    //isCollapsed: boolean;
+}
+
+const SidebarLink = ({
+    href,
+    icon: Icon,
+    label,
+    //isCollapsed
+}: SidebarlinkProps) => {
+    const pathname = usePathname();
+    const isActive = pathname === href || (pathname === "/" && href === "/dashboard");
+    const screenWidth = window.innerWidth;
+
+    const dispatch = useAppDispatch();
+    const isSidebarCollapsed = useAppSelector(
+        (state) => state.global.isSidebarCollapsed,
+    );
+
+    return (
+        <Link href={href} className='w-full'>
+            <div className={`relative flex cursor-pointer items-center gap-3 transition-colors hover:bg-gray-100 dark:bg-black dark:hover:bg-gray-700 ${
+                isActive ? "bg-gray-100 text-white dark:bg-gray-600" : ""
+            }`}>
+                {isActive && (
+                    <div className='absolute left-0 top-0 h-[100%] w-[5px] bg-blue-200' />
+                )}
+
+                <Icon className='h-6 w-6 text-gray-800 dark:text-gray-100' />
+                <span className={`font-medium text-gray-800 dark:text-gray-100`}>
+                    {label}
+                </span>
+            </div>
+        </Link>
+    )
 }
 
 export default Sidebar
