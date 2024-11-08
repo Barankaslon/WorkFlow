77b43dc7006628e78771f3702c35825e74371ff9
Alexey Gvozdev <72240384+Barankaslon@users.noreply.github.com>
2024-11-08 10:28:18 -0400

    Created teams, users, home pages

--- client/src/app/projects/[id]/page.tsx
+++ client/src/app/projects/[id]/page.tsx
@@ -1,55 +1,44 @@
-"use client"
+"use client";
 
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import ProjectHeader from "@/app/projects/ProjectHeader";
-import Board from '../BoardView';
-import List from '../ListView';
-import TimeLine from '../TimeLineView';
+import Board from "../BoardView";
+import List from "../ListView";
+import Timeline from "../TimeLineView";
+import Table from "../TableView";
+import ModalNewTask from "@/components/ModalNewTask";
 
 type Props = {
-    params: {id: string}
-}
-
-function Project({ params }: Props) {
-    const { id } = params;
-    const [ activeTab, setActiveTab ] = useState("Board");
-    const [ isModalNewTaskOpen, setIsModalNewTaskOpen ] = useState(false);
-  return (
-    <div>
-        {/*  MODAL NEW TASK */}
-        <ProjectHeader activeTab = { activeTab } setActiveTab = {setActiveTab} />
-        {activeTab === "Board" && (
-            <Board id={id} setIsModalNewTaskOpen={setIsModalNewTaskOpen} />
-        )}
-        {activeTab === "List" && (
-            <List id={id} setIsModalNewTaskOpen={setIsModalNewTaskOpen} />
-        )}
-        {activeTab === "TimeLine" && (
-            <TimeLine id={id} setIsModalNewTaskOpen={setIsModalNewTaskOpen} />
-        )}
-    </div>
-  )
+  params: { id: string };
 };
 
-type TabButtonProps = {
-    name: string;
-    icon: React.ReactNode;
-    setActiveTab: (tabName: string) => void;
-    activeTab: string;
-}
+const Project = ({ params }: Props) => {
+  const { id } = params;
+  const [activeTab, setActiveTab] = useState("Board");
+  const [isModalNewTaskOpen, setIsModalNewTaskOpen] = useState(false);
 
-const TabButton = ({name, icon,setActiveTab, activeTab}: TabButtonProps) => {
-    const isActive = activeTab === name;
-    return (
-        <button className={`relative flex items-center gap-2 px-1 py-2 
-                        text-gray-500 after:absolute after:-bottom-[9px] after:left-0 after:h-[1px] after:w-full 
-                        hover:text-blue-600 dark:text-neutral-500 dark:hover:text-white sm:px-2 lg:px-4 
-                        ${isActive ? "text-blue-600 after:bg-blue-600 dark:text-white" : ""}`} 
-                        onClick={() => setActiveTab(name)}>
-                            {icon}
-                            {name}
-                        </button>
-    )
-}
+  return (
+    <div>
+      <ModalNewTask
+        isOpen={isModalNewTaskOpen}
+        onClose={() => setIsModalNewTaskOpen(false)}
+        id={id}
+      />
+      <ProjectHeader activeTab={activeTab} setActiveTab={setActiveTab} />
+      {activeTab === "Board" && (
+        <Board id={id} setIsModalNewTaskOpen={setIsModalNewTaskOpen} />
+      )}
+      {activeTab === "List" && (
+        <List id={id} setIsModalNewTaskOpen={setIsModalNewTaskOpen} />
+      )}
+      {activeTab === "Timeline" && (
+        <Timeline id={id} setIsModalNewTaskOpen={setIsModalNewTaskOpen} />
+      )}
+      {activeTab === "Table" && (
+        <Table id={id} setIsModalNewTaskOpen={setIsModalNewTaskOpen} />
+      )}
+    </div>
+  );
+};
 
 export default Project;
