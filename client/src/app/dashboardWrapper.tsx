1e3709a33444896863179a89be5e227ec3860b03
Alexey Gvozdev <72240384+Barankaslon@users.noreply.github.com>
2024-11-16 16:30:14 -0400

    sintax error fixed

--- client/src/app/dashboardWrapper.tsx
+++ client/src/app/dashboardWrapper.tsx
@@ -19,7 +19,7 @@ const DashboardLayout = ( { children }: {children: React.ReactNode} ) => {
     }
   })
   return (
-    <div className='flex min-h-screen w-full bg-gray-50 text-gray-900'>
+    <div className='flex min-h-screen w-full bg-gray-50 text-gray-900 dark:text-white'>
         {/* SIDEBAR */}
         <Sidebar />
         <main className={`flex w-full flex-col bg-gray-50 dark:bg-dark-bg ${isSidebarCollapsed ? "" : "md:pl-64"}`}>
